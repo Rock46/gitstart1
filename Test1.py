7a1259f9c15a1a5df7bb094dd28ae3b0b9482222
Rock46 <linuxrakesh@gmail.com>
2020-06-07 13:38:45 +0530

    Made changes

--- Test1.py
+++ Test1.py
@@ -1,9 +1,6 @@
 print ("I am the Test File.")
 print ("I made another changes....... New Lines")
 print ("Added new line again")
-<<<<<<< HEAD
 print ("I added line again")
-=======
 print ("New line Added")
 
->>>>>>> f92cab2378e12912be6570c676eac461ccf9a4cc
